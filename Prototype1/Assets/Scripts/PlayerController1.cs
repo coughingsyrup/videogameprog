3de0be6bc39f4d58ffef5230edb80987d20b6a60
coughingsyrup <168101@student.vansd.org>
2022-09-23 09:11:56 -0700

    movement

--- Prototype1/Assets/Scripts/PlayerController1.cs
+++ Prototype1/Assets/Scripts/PlayerController1.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController1 : MonoBehaviour
 {
 public float speed = 5.0f;
-
+public float turnSpeed;
+public float horizontalInput;
+public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,10 @@ public float speed = 5.0f;
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-       transform.Translate(Vector3.forward * Time.deltaTime * speed); 
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+       transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
