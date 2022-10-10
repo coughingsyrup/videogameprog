7600b5b4d93814526d1af1e551bbc5bf458cd136
coughingsyrup <168101@student.vansd.org>
2022-10-10 09:19:23 -0700

    Update PlayerController.cs

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10) {
-        transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange) 
+        {
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange) 
+        {
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
